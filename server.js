c28f16ec3a68c74df9c8e2d4e480dcddb0504f40
ssachlu-max <s.sachloiou@gmail.com>
2026-04-09 20:41:48 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,20 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, 'history-quiz.html');
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(404);
+      res.end('Not found');
+      return;
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log('Quiz running at http://localhost:3000');
+});
